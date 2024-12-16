898a908f77b8eff91e350c228715e7a510774f03
Jesse-Qrtl <jessequartel@gmail.com>
2024-12-16 11:58:18 +0100

    periode 1-2

---
+++ module 3/proef/lootjes.py
@@ -0,0 +1,28 @@
+import random
+
+lootjes = []
+deelnemers = []
+
+while True:
+    naam = input('Wie doet er mee?: ').lower()
+    if naam in deelnemers:
+        print('Deze neemt had u al ingevoerd.')
+    else:
+        lootjes.append(naam)
+        deelnemers.append(naam)
+    if len(deelnemers) >= 3:
+        extra_naam = input('Doen er nogmeer mensen mee?: ').lower()
+        if extra_naam == 'nee':
+            break
+
+while True:
+    gehusseld = True
+    random.shuffle(lootjes)
+    for i in range(len(deelnemers)):
+        if deelnemers[i] == lootjes[i]:
+            gehusseld = False
+    if gehusseld:
+        break
+
+for i in range(len(deelnemers)):
+    print(f'\n{deelnemers[i]} heeft {lootjes[i]} getrokken als lootje.\n')
---
+++ module 3/proef/lootjes_pseudo.py
@@ -0,0 +1,15 @@
+# Start programma
+
+# lijsten voor de namen en lootjes
+
+# loop voor de herhaling van de vraag
+# vraag een naam
+# kijk of deze naam al in de lijst zitten
+# zo ja vraag opnieuw een naam
+# anders ga verder met het programma
+# tel hoeveel namen er in de lijst zitten
+# als er 3 of meer namen inzitten, vraag of de gebruiker nog een naam wilt toevoegen
+# zo nee stop met de loop
+
+# hussel de lootjes totdat niemnand zichzelf heeft
+# print de lootjes achter de bijbehorende namen uit
