6dc69874a75cfc36c5fc2a579a514703571fc648
Jesse-Qrtl <jessequartel@gmail.com>
2025-01-06 11:26:29 +0100

    periode 1 - 2

--- module 4/deel 1/lambda_rekenmachine/calculations.py
+++ module 4/deel 1/lambda_rekenmachine/calculations.py
@@ -1,1 +1,6 @@
-CALCULATIONS = {}
+CALCULATIONS = {
+    'addition' : lambda getal1, getal2: float(getal1 + getal2),
+    'subtraction' : lambda getal1, getal2: float(getal1 - getal2),
+    'multiplication' : lambda getal1, getal2: float(getal1 * getal2),
+    'division' : lambda getal1, getal2: float(getal1 / getal2),
+}
--- module 4/deel 1/lambda_rekenmachine/function_tests.py
+++ module 4/deel 1/lambda_rekenmachine/function_tests.py
@@ -1,23 +1,24 @@
 from functions import *
 from test_lib import *
+from calculations import *
 
 getal_1 = 3
 getal_2 = 3
 
 expected = addition(getal_1,getal_2)
-result_lambdafunction1 = addition(getal_1,getal_2)
+result_lambdafunction1 = CALCULATIONS['addition'](getal_1,getal_2)
 test('Test som functie', expected, result_lambdafunction1)
 
 expected = subtraction(getal_1,getal_2)
-result_lambdafunction2 = subtraction(getal_1,getal_2)
+result_lambdafunction2 = CALCULATIONS['subtraction'](getal_1,getal_2)
 test('Test som functie', expected, result_lambdafunction2)
 
 expected = multiplication(getal_1,getal_2)
-result_lambdafunction3 = multiplication(getal_1,getal_2)
+result_lambdafunction3 = CALCULATIONS['multiplication'](getal_1,getal_2)
 test('Test som functie', expected, result_lambdafunction3)
 
 expected = division(getal_1,getal_2)
-result_lambdafunction4 = division(getal_1,getal_2)
+result_lambdafunction4 = CALCULATIONS['division'](getal_1,getal_2)
 test('Test som functie', expected, result_lambdafunction4)
 
 report()
