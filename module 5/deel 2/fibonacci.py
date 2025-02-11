cc36926c0f9855ed27bd14b3bf9eba33e5b919fd
Jesse-Qrtl <jessequartel@gmail.com>
2025-02-11 11:23:51 +0100

    periode 2

--- module 5/deel 2/fibonacci.py
+++ module 5/deel 2/fibonacci.py
@@ -1,1 +1,10 @@
 from value_defs import *
+
+def fibonacci(amount: int) -> list:
+    lijst = []
+    if amount <= 2:
+        for i in range(amount):
+            lijst.append(i)
+        return lijst
+    for i in range(amount):
+        pass
