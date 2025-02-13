05fdd97e9d66b2130e55f08e3b6d0ae03c2b5379
Jesse-Qrtl <jessequartel@gmail.com>
2025-02-13 21:35:27 +0100

    periode 2

--- module 5/deel 2/fibonacci.py
+++ module 5/deel 2/fibonacci.py
@@ -1,10 +1,15 @@
-from value_defs import *
-
 def fibonacci(amount: int) -> list:
     lijst = []
-    if amount <= 2:
+    if amount < 1:
+        return lijst
+    elif amount <= 2:
         for i in range(amount):
             lijst.append(i)
         return lijst
-    for i in range(amount):
-        pass
+    
+    lijst = [0,1]
+    for i in range(amount - 2):
+        lijst.append(lijst[-1] + lijst[-2])
+    return lijst
+
+print(fibonacci(10))
---
+++ module 5/deel 2/password_gen.py
@@ -0,0 +1,43 @@
+import random
+import string
+
+# Genereer 2-6 hoofdletters
+amount_upper = random.randint(2,6)
+upper_positions = ''
+uppercase_letters = random.sample(string.ascii_uppercase, amount_upper)
+
+# Genereer 8-10 kleineletters
+amount_lower = random.randint(8,10)
+lowercase_letters = random.sample(string.ascii_lowercase, amount_lower)
+
+# Genereer 3 speciaale tekens
+characters = '@#$%&_?'
+special_characters = random.sample(characters, 3)
+
+# Genereer 4-7 cijfers
+amount_digits = random.randint(4,6)
+digits = random.sample(string.digits, amount_digits)
+
+# Voeg alle elementen toe en shuffle ze
+password = uppercase_letters + lowercase_letters + special_characters + digits
+random.shuffle(password)
+
+# Zoek de index van alles dat geen getal is
+non_digit_index = []
+for i in range(3, len(password)):
+    if not password[i].isdigit():
+        non_digit_index.append(i)
+
+# Wissel de getallen om die in de eerste 3 elementen staan van het wachtwoord met iets dat geen getal is
+for i in range(3):
+    if password[i].isdigit():
+        swap_index = random.choice(non_digit_index)
+        password[i], password[swap_index] = password[swap_index], password[i]
+        non_digit_index.remove(swap_index)
+
+# Wissel het laatste getal om als het een kleine letter is
+for i in range(len(password) - 1):
+    if password[-1].islower() and not password[i].islower():
+        password[-1], password[i] = password[i], password[-1]
+        break
+
