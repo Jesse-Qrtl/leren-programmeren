115288e86d68a2133b3719b4ede36a17f8818605
Jesse-Qrtl <jessequartel@gmail.com>
2025-03-24 09:56:40 +0100

    periode 3

--- module 5/deel 3/lootjes.py
+++ module 5/deel 3/lootjes.py
@@ -1,44 +1,51 @@
 import random
 
-lootjes = []
-deelnemers = {}
+deelnemers = []
+cadeautjes = {}
 
 while True:
-    naam = input('Wie doet er mee?: ').lower()
+    naam = input('Wie doet er mee?: ').strip().lower()
     if naam in deelnemers:
         print('Deze neemt had u al ingevoerd.')
     else:
-        cadeautjes = []
-        for i in range(3):
-            cadeau = input(f'Wat voor cadeau wil je? {i + 1}: ')
-            cadeautjes.append(cadeau)
-        deelnemers[naam] = {
-            "cadeau" : cadeautjes
-        }
-        lootjes.append(naam)
+        deelnemers.append(naam)
+
+        wensen = []
+        print(f'Wat zijn de drie cadeautjes die {naam} graag zou willen ontvangen?')
+        for i in range(1, 4):
+            cadeau = input(f'Cadeautje {i}: ').strip()
+            wensen.append(cadeau)
+        cadeautjes[naam] = wensen
+
     if len(deelnemers) >= 3:
-        extra_naam = input('Doen er nogmeer mensen mee?: ').lower()
+        extra_naam = input('Doen er nogmeer mensen mee?: ').strip().lower()
         if extra_naam == 'nee':
             break
 
-while True:
-    gehusseld = True
-    random.shuffle(lootjes)
-    for i, naam in enumerate(deelnemers):
-        for i in range(len(deelnemers)):
-            if deelnemers[lootjes[i]] == lootjes[i]:
-                gehusseld = False
-    if gehusseld:
-        break
+lootjes = deelnemers[:]
+random.shuffle(lootjes)
 
 while True:
-    print(f'\n{deelnemers}')
-    check_naam = input('Welke naam wil je checken?: ').lower()
-    if check_naam in deelnemers:
-        index_naam = deelnemers.index(check_naam)
-        print(f'\n{deelnemers[check_naam]} heeft {lootjes[index_naam]} getrokken als lootje.')
-        verder_checken = input('Wil je nog een naam checken?: ').lower()
-        if verder_checken == 'nee':
+    for i in range(len(deelnemers)):
+        if deelnemers[i] == lootjes[i]:
+            random.shuffle(lootjes)
             break
     else:
-        print('Deze persoon doet niet mee.')
+        break
+
+
+print(cadeautjes)
+while True:
+    print(f'\n{deelnemers}')
+    naam_checken = input('Welke naam wil je checken?: ').strip().lower()
+    if naam_checken in deelnemers:
+        index = deelnemers.index(naam_checken)
+        lootje = lootjes[index]
+        
+        print(f"\n{naam_checken} heeft {lootje} getrokken als lootje.")
+        print(f"Wensenlijst van {lootje}: {cadeautjes[lootje]}")
+    else:
+        print("Deze persoon doet niet mee.")
+    DoorMet_checken = input('Wil je nog een naam checken?: ').strip().lower()
+    if DoorMet_checken == 'nee':
+        break
